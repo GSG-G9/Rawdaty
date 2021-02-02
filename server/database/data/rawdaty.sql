2387aa5f4c77ff3ccab7dd184a0b0eadff584d89
Israa91 <eng.israa.1991@gmail.com>
2021-02-02 12:34:49 +0200

    database scheme error

--- server/database/data/rawdaty.sql
+++ server/database/data/rawdaty.sql
@@ -1,14 +1,28 @@
 BEGIN;
 
-DROP TABLE IF EXISTS kindergarten , user, location, comments CASCADE;
+DROP TABLE IF EXISTS kindergarten , users, locations, comments CASCADE;
 
-CREATE TABLE kindergarten(
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+users_name VARCHAR(50) NOT NULL,
+email VARCHAR(255) NOT NULL,
+password TEXT NOT NULL,
+is_admin VARCHAR(50)  DEFAULT 'false',
+create_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE locations(
+id SERIAL PRIMARY KEY,
+location_name TEXT NOT NULL
+);
+
+CREATE TABLE kindergartens(
 id SERIAL PRIMARY KEY,
 kindergarten_name VARCHAR(255)	NOT NULL,
-user_id	SERIAL	REFERENCES user(id),
+users_id	SERIAL	REFERENCES users(id),
 cover_image	TEXT NOT NULL,
 description	TEXT NOT NULL,
-location_id	SERIAL	REFERENCES location(id),
+location_id	SERIAL	REFERENCES locations(id),
 phone_number INTEGER NOT NULL,
 latitude Decimal(8,6),
 longitude Decimal(9,6),
@@ -17,27 +31,13 @@ max_price FLOAT,
 periods	TEXT,	
 images_gallery TEXT,	
 request_status VARCHAR(50),	
-is_enable VARCHAR(50) SET DEFAULT 'false',locationENT_TIMESTAMP
-);
-
-CREATE TABLE location(
-id SERIAL PRIMARY KEY,
-location_name TEXT NOT NULL
-);
-
-CREATE TABLE user(
-id SERIAL PRIMARY KEY,
-user_name VARCHAR(50) NOT NULL,
-email VARCHAR(255) NOT NULL,
-password TEXT NOT NULL,
-is_admin VARCHAR(50) SET DEFAULT 'false',
-create_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+is_enable VARCHAR(50)  DEFAULT 'false'
 );
 
 CREATE TABLE comments(
 id SERIAL PRIMARY KEY,
-kindergarten_id	INTEGER REFERENCES Kindergarten(id)ON UPDATE CASCADE ON DELETE CASCADE,
-user_name VARCHAR(50) NOT NULL,
+kindergarten_id	INTEGER REFERENCES kindergartens(id)ON UPDATE CASCADE ON DELETE CASCADE,
+users_name VARCHAR(50) NOT NULL,
 comment	TEXT NOT NULL,
 rating DECIMAL	NOT NULL,
 create_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
