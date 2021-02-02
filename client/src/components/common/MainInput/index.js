e98f82b32489dd4244a1048d412368eb95419ba1
Osama-you <osama.w.younis@gmail.com>
2021-02-02 18:35:36 +0200

    add value and onChange for the props and display slider value Relates #49

--- client/src/components/common/MainInput/index.js
+++ client/src/components/common/MainInput/index.js
@@ -5,21 +5,39 @@ import 'antd/dist/antd.css'; // use for RangePicker style
 
 import './style.css';
 
-const MainInput = ({ type, width, height, placeholder, min, max }) => {
-  const [sliderValue, setSliderValue] = useState([]);
+const MainInput = ({
+  type,
+  width,
+  height,
+  placeholder,
+  min,
+  max,
+  value,
+  onChange,
+}) => {
+  const [sliderValue, setSliderValue] = useState([min, max]);
   const { TextArea } = Input;
   const { RangePicker } = TimePicker;
 
   const style = { width, height };
 
-  const sliderOnChange = (value) => {
-    setSliderValue(value);
+  const sliderOnChange = (val) => {
+    setSliderValue(val);
+    onChange(val);
   };
-  const inputLabel = <span>اسم الروضة</span>;
+
+  const inputLabel = <span className="input-label">اسم الروضة</span>;
   let input;
   switch (type) {
     case 'text':
-      input = <Input placeholder={placeholder} style={style} />;
+      input = (
+        <Input
+          placeholder={placeholder}
+          style={style}
+          defaultValue={value}
+          onChange={onChange}
+        />
+      );
       break;
     case 'textArea':
       input = (
@@ -27,24 +45,31 @@ const MainInput = ({ type, width, height, placeholder, min, max }) => {
           placeholder={placeholder}
           autoSize={{ minRows: 3, maxRows: 5 }}
           style={style}
+          defaultValue={value}
+          onChange={onChange}
         />
       );
       break;
     case 'date':
-      input = <RangePicker style={style} />;
+      input = (
+        <RangePicker style={style} defaultValue={value} onChange={onChange} />
+      );
       break;
     case 'rangeSlider':
       input = (
         <div>
-          <Slider
-            range
-            min={min}
-            max={max}
-            defaultValue={[min, max]}
-            style={style}
-            onChange={sliderOnChange}
-          />
-          {sliderValue}
+          <div className="Slider">
+            <Slider
+              range
+              min={min}
+              max={max}
+              value={sliderValue}
+              style={{ width }}
+              defaultValue={value}
+              onChange={sliderOnChange}
+            />
+            <span className="slider-label">{`${sliderValue[0]} - ${sliderValue[1]} ₪`}</span>
+          </div>
         </div>
       );
       break;
@@ -65,6 +90,8 @@ MainInput.defaultProps = {
   placeholder: '',
   min: 0,
   max: 900,
+  value: '',
+  onChange: () => {},
 };
 
 MainInput.propTypes = {
@@ -74,6 +101,8 @@ MainInput.propTypes = {
   height: PropTypes.string,
   min: PropTypes.number,
   max: PropTypes.number,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default MainInput;
