8b502907571b8348a7044a6754a7097ad25660fe
Osama-you <osama.w.younis@gmail.com>
2021-02-02 08:23:11 +0200

    Remove router componant from app.js Relates #4

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import Arabic from 'antd/lib/locale/ar_EG';
 
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <ConfigProvider direction="rtl" locale={Arabic}>
-      <App />
-    </ConfigProvider>
-  </Router>,
+  <ConfigProvider direction="rtl" locale={Arabic}>
+    <App />
+  </ConfigProvider>,
   document.getElementById('root')
 );
