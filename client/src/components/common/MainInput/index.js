ec50006fcb973fe1d48481bc8926160488b39319
Osama-you <osama.w.younis@gmail.com>
2021-02-02 17:23:36 +0200

    add RangePicker & sliderValue type input and inputLabel Relates #79

--- client/src/components/common/MainInput/index.js
+++ client/src/components/common/MainInput/index.js
@@ -1,33 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Input } from 'antd';
+import { Input, TimePicker, Slider } from 'antd';
+import 'antd/dist/antd.css'; // use for RangePicker style
 
 import './style.css';
 
-const MainInput = ({ type, width, height, placeholder }) => {
+const MainInput = ({ type, width, height, placeholder, min, max }) => {
+  const [sliderValue, setSliderValue] = useState([]);
   const { TextArea } = Input;
+  const { RangePicker } = TimePicker;
+
   const style = { width, height };
 
+  const sliderOnChange = (value) => {
+    setSliderValue(value);
+  };
+  const inputLabel = <span>اسم الروضة</span>;
+  let input;
   switch (type) {
     case 'text':
-      return <Input placeholder={placeholder} style={style} />;
+      input = <Input placeholder={placeholder} style={style} />;
+      break;
     case 'textArea':
-      return (
+      input = (
         <TextArea
           placeholder={placeholder}
           autoSize={{ minRows: 3, maxRows: 5 }}
           style={style}
         />
       );
+      break;
+    case 'date':
+      input = <RangePicker style={style} />;
+      break;
+    case 'rangeSlider':
+      input = (
+        <div>
+          <Slider
+            range
+            min={min}
+            max={max}
+            defaultValue={[min, max]}
+            style={style}
+            onChange={sliderOnChange}
+          />
+          {sliderValue}
+        </div>
+      );
+      break;
     default:
-      return <></>;
+      input = <></>;
   }
+  return (
+    <div className="Input">
+      {inputLabel}
+      {input}
+    </div>
+  );
 };
 
 MainInput.defaultProps = {
-  width: '240px',
-  height: '28px',
+  width: '300px',
+  height: '40px',
   placeholder: '',
+  min: 0,
+  max: 900,
 };
 
 MainInput.propTypes = {
@@ -35,6 +72,8 @@ MainInput.propTypes = {
   placeholder: PropTypes.string,
   width: PropTypes.string,
   height: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
 };
 
 export default MainInput;
