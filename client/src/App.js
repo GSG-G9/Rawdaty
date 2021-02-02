c336893d135fb30c7cb2a0cf98d9b6b4540a3ade
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-02 17:42:39 +0200

    removing MainButton component form app Relates #33

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import MainButton from './component/common/MainButton';
-
 import './App.css';
 
 const App = () => (
   <Router>
-    <div className="App">
-      <MainButton
-        backgroundColor="#739c98"
-        border="3px solid #69938f"
-        color="#fff"
-        padding="9px 15px"
-        Buttontext="Submit"
-      />
-    </div>
+    <div className="App" />
   </Router>
 );
 
