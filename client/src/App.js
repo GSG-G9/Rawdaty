41a93f15002bdfc190ce8e630e3e90819ea6e341
Israa91 <eng.israa.1991@gmail.com>
2021-02-02 18:08:47 +0200

    remove rating component from App.js file after I finsh testing relates #40

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Rating from './components/common/Rating';
 
 import './App.css';
 
 const App = () => (
   <Router>
-    <div className="App">
-      روضتي هنا
-      <Rating />
-    </div>
+    <div className="App">روضتي هنا</div>
   </Router>
 );
 
