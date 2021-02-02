69d9c93e33d6071274318c8c37a53a7e79f681b4
Osama-you <osama.w.younis@gmail.com>
2021-02-02 10:30:05 +0200

    add Router to the react compenant Relaets #4

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
 
-const App = () => <div className="App">روضتي هنا</div>;
+const App = () => (
+  <Router>
+    <div className="App">روضتي هنا</div>
+  </Router>
+);
 
 export default App;
