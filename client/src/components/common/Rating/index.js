820e6331505cd95224dd06cd52db39a7891c9cbc
Israa91 <eng.israa.1991@gmail.com>
2021-02-02 17:15:51 +0200

    Rating component relates #40

---
+++ client/src/components/common/Rating/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import 'antd/dist/antd.css';
+import { Rate } from 'antd';
+
+import './style.css';
+
+const Rating = ({ rateValue }) => {
+  const [value, setValue] = useState(0);
+  const desc = ['رهيب', 'سيء', 'عادي', 'جيد', 'رائع'];
+
+  const handleChange = (val) => {
+    setValue(val);
+  };
+
+  return (
+    <span>
+      {rateValue === -1 ? (
+        <Rate tooltips={desc} onChange={handleChange} value={value} />
+      ) : (
+        <Rate disabled value={rateValue} />
+      )}
+    </span>
+  );
+};
+
+Rating.defaultProps = {
+  rateValue: -1,
+};
+
+Rating.propTypes = {
+  rateValue: PropTypes.number,
+};
+
+export default Rating;
