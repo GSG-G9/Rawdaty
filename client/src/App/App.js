9e74f2be4f68927c03234c86493e8a832736be75
Israa91 <eng.israa.1991@gmail.com>
2021-02-03 22:32:48 +0200

    remove the import of card from App.js file after I finished testing relates #37

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CardComponent from '../Components/Common/Card';
+
 import './App.css';
 
 const App = () => (
   <Router>
     <div className="App">روضتي هنا</div>
-    <CardComponent
-      title="روضة الامل المشرق"
-      rating={2}
-      reviewersNo={10}
-      location="غزة-فلسطين"
-      price="200-500 شيكل"
-      cover="https://thumbs.dreamstime.com/b/group-preschool-children-engaged-handcrafts-group-preschool-kids-engaged-handcrafts-children-teacher-classroom-148143087.jpg"
-      kindergartenId={1}
-    />
   </Router>
 );
 
