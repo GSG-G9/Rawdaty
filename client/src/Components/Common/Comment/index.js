614785c4526d8d8d8131c6ded11d5be34edbfae4
Osama-you <osama.w.younis@gmail.com>
2021-02-03 21:16:11 +0200

    done the structuring of the component and its style Relates #41

--- client/src/Components/Common/Comment/index.js
+++ client/src/Components/Common/Comment/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Avatar, Row, Col } from 'antd';
+import PropTypes from 'prop-types';
+import Rating from '../Rating';
+import MainButton from '../MainButton';
 
 import './style.css';
 
-const Comment = () => <div />;
+const Comment = ({
+  id,
+  isAdmin,
+  userName,
+  maxWidth,
+  commentText,
+  date,
+  rateValue,
+  onAdminClick,
+}) => (
+  <div id={id} className="Comment" style={{ maxWidth }}>
+    <Row justify="start">
+      <Col id="avatar" span={4}>
+        <Avatar size={64} name={userName} />
+        {/* <UserImage userName={userName} /> */}
+      </Col>
+      <Col span={20}>
+        <Col span={24}>
+          <Rating rateValue={rateValue} />
+          <span id="comment-date">{date}</span>
+          {isAdmin && (
+            <MainButton
+              id="deleteBtn"
+              isDelete
+              size="small"
+              onClick={onAdminClick}
+            >
+              حدف
+            </MainButton>
+          )}
+        </Col>
+        <Col span={24}>
+          <p id="Comment-des">{commentText}</p>
+        </Col>
+      </Col>
+    </Row>
+  </div>
+);
+
+Comment.defaultProps = {
+  isAdmin: false,
+  maxWidth: '500px',
+  onAdminClick: () => {},
+};
+
+Comment.propTypes = {
+  id: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  commentText: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  rateValue: PropTypes.number.isRequired,
+  isAdmin: PropTypes.bool,
+  maxWidth: PropTypes.string,
+  onAdminClick: PropTypes.func,
+};
 
 export default Comment;
