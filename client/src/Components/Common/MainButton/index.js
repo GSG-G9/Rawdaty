d11f1ad56aff132f49b223a1ce73561f2cd2ce75
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-03 16:28:22 +0200

    romove folder component and move MainButton folder to Componenets Folder Relates #33

--- client/src/Components/Common/MainButton/index.js
+++ client/src/Components/Common/MainButton/index.js
@@ -14,6 +14,7 @@ const MainButton = ({ onClick, children, isDelete, ...otherStyleProps }) => (
   </Button>
 );
 MainButton.defaultProps = {
+  children: 'إرسال',
   backgroundColor: 'var(--main-color)',
   border: 'var(--button-border)',
   color: 'var(--main-white)',
@@ -22,7 +23,7 @@ MainButton.defaultProps = {
 
 MainButton.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.string.isRequired,
+  children: PropTypes.string,
   isDelete: PropTypes.bool,
   backgroundColor: PropTypes.string,
   border: PropTypes.string,
