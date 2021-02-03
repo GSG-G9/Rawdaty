a51f471e6c44c6cdb1373d3f3de998da52f8caa2
Alaa Alser <alaalsser@gmail.com>
2021-02-03 19:57:43 +0200

    Add loctoin icon for search case & change app.js to index.js and App.css to style.css Relates #53

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import './style.css';
 
 const App = () => (
   <Router>
--- client/src/Components/Common/DropList/index.js
+++ client/src/Components/Common/DropList/index.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import { Select } from 'antd';
 import PropTypes from 'prop-types';
+import icon from './Vector.svg';
 
 const { Option } = Select;
 
-const DropList = ({ options, onChange }) => (
-  <Select
-    onChange={onChange}
-    labelInValue
-    defaultValue={{ value: 1 }}
-    style={{ width: 130 }}
-  >
-    {options.map((option) => (
-      <Option value={option.value} disabled={option.disabled}>
-        {option.label}
-      </Option>
-    ))}
-  </Select>
+const DropList = ({ options, isSearch, ...otherSelectProps }) => (
+  <>
+    {isSearch ? (
+      <Select
+        labelInValue
+        style={{ width: 300, height: 19 }}
+        {...otherSelectProps}
+        suffixIcon={<img src={icon} alt="location" />}
+      >
+        {options.map((option) => (
+          <Option
+            key={option.id}
+            value={option.value}
+            disabled={option.disabled}
+          >
+            {option.label}
+          </Option>
+        ))}
+      </Select>
+    ) : (
+      <Select labelInValue style={{ width: 300 }} {...otherSelectProps}>
+        {options.map((option) => (
+          <Option
+            key={option.id}
+            value={option.value}
+            disabled={option.disabled}
+          >
+            {option.label}
+          </Option>
+        ))}
+      </Select>
+    )}
+  </>
 );
 
 DropList.propTypes = {
   options: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onChange: PropTypes.func.isRequired,
+  isSearch: PropTypes.bool.isRequired,
 };
 
 export default DropList;
