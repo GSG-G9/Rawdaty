d294195cd72784a8d69cd1d7cf1542dfc0d28676
Israa91 <eng.israa.1991@gmail.com>
2021-02-03 10:38:55 +0200

    add setRating props to the Rating component Relates #40

--- client/src/components/common/Rating/index.js
+++ client/src/components/common/Rating/index.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Rate } from 'antd';
 
-const Rating = ({ rateValue }) => {
+const Rating = ({ rateValue, setRating }) => {
   const [value, setValue] = useState(0);
   const desc = ['رهيب', 'سيء', 'عادي', 'جيد', 'رائع'];
 
   const handleChange = (val) => {
     setValue(val);
+    setRating(val);
   };
 
   return (
@@ -23,10 +24,12 @@ const Rating = ({ rateValue }) => {
 
 Rating.defaultProps = {
   rateValue: -1,
+  setRating: () => {},
 };
 
 Rating.propTypes = {
   rateValue: PropTypes.number,
+  setRating: PropTypes.func,
 };
 
 export default Rating;
