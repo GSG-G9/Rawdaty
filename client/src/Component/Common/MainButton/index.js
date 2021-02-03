c421e0377d6474597b795f23886031bd66078133
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-03 13:59:04 +0200

    remove unused import form app

--- client/src/Component/Common/MainButton/index.js
+++ client/src/Component/Common/MainButton/index.js
@@ -2,30 +2,31 @@ import React from 'react';
 import { Button } from 'antd';
 import PropTypes from 'prop-types';
 
-const MainButton = ({ onClick, children, ...otherStyleProps }) => (
+const MainButton = ({ onClick, children, isDelete, ...otherStyleProps }) => (
   <Button
     type="primary"
     size="large"
     onClick={onClick}
-    style={{ ...otherStyleProps }}
+    style={!isDelete ? { ...otherStyleProps } : null}
+    danger={isDelete}
   >
     {children}
   </Button>
 );
 MainButton.defaultProps = {
-  backgroundColor: '#739C98',
-  border: '3px solid #69938F',
-  color: '#fff',
-  padding: '0px 15px',
+  backgroundColor: 'var(--main-color)',
+  border: 'var(--button-border)',
+  color: 'var(--main-white)',
   onClick: () => {},
+  isDelete: false,
 };
 
 MainButton.propTypes = {
   onClick: PropTypes.func,
   children: PropTypes.string.isRequired,
+  isDelete: PropTypes.bool,
   backgroundColor: PropTypes.string,
   border: PropTypes.string,
   color: PropTypes.string,
-  padding: PropTypes.string,
 };
 export default MainButton;
