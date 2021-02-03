5efefe3a3096af957f84fcdad0ea6019102bca47
Israa91 <eng.israa.1991@gmail.com>
2021-02-03 10:50:52 +0200

    add new folder App and put App.js and App.css inside it and change vscode settings formatOnSave to false Relates #40

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import Arabic from 'antd/lib/locale/ar_EG';
 
-import App from './App';
+import App from './App/App';
 
 ReactDOM.render(
   <ConfigProvider direction="rtl" locale={Arabic}>
