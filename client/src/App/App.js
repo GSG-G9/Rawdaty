b64459dc8b8028f6d51a14480305453f5b650580
Alaa Alser <alaalsser@gmail.com>
2021-02-03 15:20:32 +0200

    hangling onChanglge value Relates #53

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import DropList from '../Components/Common/DropList';
 
 import './App.css';
 
 const App = () => (
   <Router>
     <div className="App">روضتي هنا</div>
+    <DropList
+      options={[
+        { label: 'جميع المناطق', value: 1, disabled: true },
+        { label: 'غزة', value: 2 },
+        { label: 'خانيونس', value: 3 },
+        { label: 'رفح', value: 4 },
+        { label: 'نصيرات', value: 5 },
+        { label: 'البريج', value: 6 },
+        { label: 'القرارة', value: 7 },
+        { label: 'المغازي', value: 8 },
+        { label: 'المصدر', value: 9 },
+      ]}
+      onChange={(VAl) => console.log(VAl)}
+    />
   </Router>
 );
 
--- client/src/Components/Common/DropList/index.js
+++ client/src/Components/Common/DropList/index.js
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types';
 
 const { Option } = Select;
 
-function DropList({ options }) {
-  return (
-    <Select labelInValue defaultValue={{ value: 1 }} style={{ width: 130 }}>
-      {options.map((option) => (
-        <Option value={option.value}>{option.label}</Option>
-      ))}
-    </Select>
-  );
-}
+const DropList = ({ options, onChange }) => (
+  <Select
+    onChange={onChange}
+    labelInValue
+    defaultValue={{ value: 1 }}
+    style={{ width: 130 }}
+  >
+    {options.map((option) => (
+      <Option value={option.value} disabled={option.disabled}>
+        {option.label}
+      </Option>
+    ))}
+  </Select>
+);
+
+DropList.defaultProps = {
+  onChange: () => {},
+};
 
 DropList.propTypes = {
   options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onChange: PropTypes.func,
 };
 
 export default DropList;
