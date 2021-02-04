fd2212218a2ebd942c3dc4987c309e24a538d359
Alaa Alser <alaalsser@gmail.com>
2021-02-04 23:13:44 +0200

    solve conflect Relates #35

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 import NavTap from '../Components/Common/NavTap';
+import './style.css';
 
 const App = () => (
   <Router>
