f5fadd577e5c237dd240a2b7b182fa763911528a
Israa91 <eng.israa.1991@gmail.com>
2021-02-04 17:42:27 +0200

    Change the price to minPrice and maxPrice and change the proptypes to number relates #37

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CardComponent from '../Components/Common/Card';
+
 import './style.css';
 
 const App = () => (
   <Router>
     <div className="App">روضتي هنا</div>
-    <CardComponent
-      title=" النموذجية الخاصة روضة الامل المشرق"
-      rating={4}
-      reviewersNo={10}
-      location="غزة-فلسطين"
-      minPrice={200}
-      maxPrice={600}
-      cover="https://thumbs.dreamstime.com/b/group-preschool-children-engaged-handcrafts-group-preschool-kids-engaged-handcrafts-children-teacher-classroom-148143087.jpg"
-      kindergartenId={1}
-    />
   </Router>
 );
 
