03a3903a895ea71a5104df0c17d011f145ebcf21
Alaa Alser <alaalsser@gmail.com>
2021-02-04 02:40:14 +0200

    Refacot the code Relats #53

--- client/src/Components/Common/DropList/index.js
+++ client/src/Components/Common/DropList/index.js
@@ -1,43 +1,23 @@
 import React from 'react';
 import { Select } from 'antd';
 import PropTypes from 'prop-types';
-import icon from './Vector.svg';
+import locationIcon from './Vector.svg';
 
 const { Option } = Select;
 
 const DropList = ({ options, isSearch, ...otherSelectProps }) => (
-  <>
-    {isSearch ? (
-      <Select
-        labelInValue
-        style={{ width: 300, height: 19 }}
-        {...otherSelectProps}
-        suffixIcon={<img src={icon} alt="location" />}
-      >
-        {options.map((option) => (
-          <Option
-            key={option.id}
-            value={option.value}
-            disabled={option.disabled}
-          >
-            {option.label}
-          </Option>
-        ))}
-      </Select>
-    ) : (
-      <Select labelInValue style={{ width: 300 }} {...otherSelectProps}>
-        {options.map((option) => (
-          <Option
-            key={option.id}
-            value={option.value}
-            disabled={option.disabled}
-          >
-            {option.label}
-          </Option>
-        ))}
-      </Select>
-    )}
-  </>
+  <Select
+    labelInValue
+    style={{ width: 300, height: 19 }}
+    {...otherSelectProps}
+    suffixIcon={isSearch ? <img src={locationIcon} alt="location" /> : ''}
+  >
+    {options.map((option) => (
+      <Option key={option.id} value={option.value} disabled={option.disabled}>
+        {option.label}
+      </Option>
+    ))}
+  </Select>
 );
 
 DropList.propTypes = {
