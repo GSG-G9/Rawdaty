9b8939b955724270d091179eedf88e3d2129f767
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-04 12:30:57 +0200

    create component User image to add the user image if added or to add the image for user with first letter of their name by using Avatar from ant design, I added some styles css for the avatar Relates #54

---
+++ client/src/Components/Common/UserImage/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Avatar } from 'antd';
+import PropTypes from 'prop-types';
+
+import './style.css';
+
+const UserImage = ({ userName, image, background, fontSize, color }) => (
+  <div>
+    <Avatar
+      size={45}
+      className="user-image"
+      style={{ background, fontSize, color }}
+      src={image || ' '}
+    >
+      {image && userName.charAt(0)}
+    </Avatar>
+  </div>
+);
+
+UserImage.defaultProps = {
+  background: '#B76E6E',
+  fontSize: '20px',
+  color: '#fff',
+  image: ' ',
+};
+
+UserImage.propTypes = {
+  userName: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  background: PropTypes.string,
+  fontSize: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default UserImage;
