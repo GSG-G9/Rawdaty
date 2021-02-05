55f4acd7228f482a0c278a9dd7ed2341b5d3369a
Osama-you <osama.w.younis@gmail.com>
2021-02-06 01:47:09 +0200

    remove maxWidth prop and replace comment-date div to h3 Relates #41

--- client/src/Components/Common/Comment/index.js
+++ client/src/Components/Common/Comment/index.js
@@ -12,14 +12,13 @@ const { Title, Text } = Typography;
 const Comment = ({
   isAdmin,
   userName,
-  maxWidth,
   commentText,
   date,
   rateValue,
   onAdminClick,
   ...otherProps
 }) => (
-  <div className="Comment" style={{ maxWidth }} {...otherProps}>
+  <div className="Comment" {...otherProps}>
     <Row justify="start">
       <Col id="avatar" span={5}>
         <Row>
@@ -30,7 +29,9 @@ const Comment = ({
       <Col span={19}>
         <Row className="top-container">
           <Rating rateValue={rateValue} />
-          <div id="comment-date">{date}</div>
+          <Title level={3} id="comment-date">
+            {date}
+          </Title>
           {isAdmin && (
             <MainButton
               id="deleteBtn"
@@ -52,7 +53,6 @@ const Comment = ({
 
 Comment.defaultProps = {
   isAdmin: false,
-  maxWidth: '500px',
   onAdminClick: () => {},
 };
 
@@ -62,7 +62,6 @@ Comment.propTypes = {
   date: PropTypes.string.isRequired,
   rateValue: PropTypes.number.isRequired,
   isAdmin: PropTypes.bool,
-  maxWidth: PropTypes.string,
   onAdminClick: PropTypes.func,
 };
 
