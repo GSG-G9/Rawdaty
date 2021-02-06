1d6f432d89f4fd792fcf96ecb0996a377a7630f5
Israa91 <eng.israa.1991@gmail.com>
2021-02-06 21:07:05 +0200

    start creating container

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import CommentContainer from '../Components/Common/CommentContainer';
 import './style.css';
 
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
 const App = () => (
   <Router>
     <div className="App">روضتي هنا</div>
+    <CommentContainer data={data} />
   </Router>
 );
 
---
+++ client/src/Components/Common/CommentContainer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { List } from 'antd';
+import PropTypes from 'prop-types';
+import Comment from '../Comment';
+
+const CommentContainer = ({ data }) => (
+  <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={(item) => (
+      //   <List.Item>
+      //     <List.Item.Meta
+      //       title={<a href="https://ant.design">{item.title}</a>}
+      //       description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+      //     />
+      //   </List.Item>
+      <Comment
+        isAdmin
+        userName="حنين سامي"
+        commentText="هذه روضة رائعة اتمنى لها دوام التفوق والنجاح"
+      />
+    )}
+  />
+);
+
+export default CommentContainer;
