257f063945a10e493159f20fd43dffdd00633e67
Alaa Alser <alaalsser@gmail.com>
2021-02-06 13:24:18 +0200

    Add about and search icons and radio buttons Relates #35

--- client/src/Components/Common/NavTap/index.js
+++ client/src/Components/Common/NavTap/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import './style.css';
 import { Radio } from 'antd';
 import { ReactComponent as Home } from '../../../assets/icons/home.svg';
+import { ReactComponent as About } from '../../../assets/icons/about.svg';
+import { ReactComponent as Search } from '../../../assets/icons/search.svg';
 
 const NavTap = () => (
   <Radio.Group buttonStyle="solid">
-    <Radio.Button
-      style={{
-        width: 110,
-        textAlign: 'center',
-      }}
-      value="1"
-    >
+    <Radio.Button value="1">
       <Home className="home" />
       الرئيسية
     </Radio.Button>
+    <Radio.Button value="2">
+      <About className="about" />
+      حول
+    </Radio.Button>
+    <Radio.Button value="3">
+      <Search className="search" />
+      البحث
+    </Radio.Button>
   </Radio.Group>
 );
+// NavTap.propTypes = { text: PropTypes.string.isRequired };
+
 export default NavTap;
