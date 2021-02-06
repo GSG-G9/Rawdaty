1594dc01e4f4caf13e52ef626f246d59e38f67de
Israa91 <eng.israa.1991@gmail.com>
2021-02-06 09:58:29 +0200

    use footer layout from antd and change the icons Relates #42

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import FooterComponent from '../Components/Common/Footer';
 
 import './style.css';
 
 const App = () => (
   <Router>
-    <div className="App" />
-    <FooterComponent />
     <div className="App">روضتي هنا</div>
   </Router>
 );
--- client/src/Components/Common/Footer/index.js
+++ client/src/Components/Common/Footer/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  FacebookFilled,
-  InstagramFilled,
-  TwitterSquareFilled,
-} from '@ant-design/icons';
+import { Layout } from 'antd';
+
+import facebook from './assets/icons/facebook.svg';
+import instagram from './assets/icons/instagram.svg';
+import twitter from './assets/icons/twitter.svg';
 
 import './style.css';
 
+const { Footer } = Layout;
 const FooterComponent = () => (
-  <div className="footer">
+  <Footer className="footer" style={{ padding: '0' }}>
     <span className="icons">
       <Link to={{ pathname: 'https://www.instagram.com/' }} target="_blank">
-        <InstagramFilled className="icon" />
+        <img src={instagram} alt="instagram" className="icon" />
       </Link>
       <Link to={{ pathname: 'https://twitter.com/' }} target="_blank">
-        <TwitterSquareFilled className="icon" />{' '}
+        <img src={twitter} alt="twitter" className="icon" />
       </Link>
       <Link to={{ pathname: 'https://www.facebook.com/' }} target="_blank">
-        <FacebookFilled className="icon" />{' '}
+        <img src={facebook} alt="facebook" className="icon" />
       </Link>
+      <span className="copy-write"> جميع الحقوق محفوظة2021Ⓒ</span>
     </span>
-    <span className="copy-write">جميع الحقوق محفوظة 2021Ⓒ</span>
-  </div>
+  </Footer>
 );
 
 export default FooterComponent;
