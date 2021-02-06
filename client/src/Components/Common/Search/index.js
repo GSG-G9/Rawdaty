3130e33145e64f0034d6ca9f3772754ecdf12190
Osama-you <osama.w.younis@gmail.com>
2021-02-06 17:30:54 +0200

    add (DorpList, MainInput, MainButton) components with same style Relates #39

--- client/src/Components/Common/Search/index.js
+++ client/src/Components/Common/Search/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from 'antd';
+
+import DorpList from '../DropList';
+import MainInput from '../MainInput';
+import MainButton from '../MainButton';
+import price from '../../../assets/icons/price.svg';
 
 import './style.css';
 
-const Search = () => <div className="search-container" />;
+const { Title } = Typography;
 
+const Search = ({ options, onClick }) => {
+  console.log(options);
+  return (
+    <div className="search-container">
+      <div className="search-options">
+        <DorpList options={options} isSearch />
+        <img className="price-icon" src={price} alt="price" />
+        <Title className="price-text" level={5}>
+          السعر
+        </Title>
+        <MainInput type="rangeSlider" width="200px" />
+      </div>
+      <MainInput type="search" height="52px" placeholder="أدخل إسم الروضة" />
+      <MainButton
+        onClick={onClick}
+        height="52px"
+        border="1.6px solid var(--main-gray)"
+      >
+        بحث
+      </MainButton>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 export default Search;
