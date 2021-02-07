af837a192cc69eacdbc821eac6e09fb62d02bacc
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-07 10:34:58 +0200

    adding userImage component

--- client/src/Components/Common/Navbar/index.js
+++ client/src/Components/Common/Navbar/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
 // import PropTypes from 'prop-types';
-import MainButton from '../MainButton';
-
+// import MainButton from '../MainButton';
+import userImage from '../UserImage';
 import './style.css';
 
-function Navbar() {
+function Navbar({ userName }) {
   const menu = (
     <Menu>
       <Menu.Item key="0">
-        <a href="http://www.alipay.com/">1st menu item</a>
-      </Menu.Item>
-      <Menu.Item key="1">
-        <a href="http://www.taobao.com/">2nd menu item</a>
+        <p>{userName}</p>
       </Menu.Item>
       <Menu.Divider />
-      <Menu.Item key="3">3rd menu item</Menu.Item>
+      <Menu.Item key="1">
+        <a href="http://www.taobao.com/">صفحة الروضة</a>
+      </Menu.Item>
+
+      <Menu.Item key="3">تسجيل الخروج</Menu.Item>
     </Menu>
   );
   return (
@@ -25,24 +27,20 @@ function Navbar() {
         <h1>روضــتـــي</h1>
       </div>
       <div className="navbar-taps-div">
-        <div>Component</div>
-        <MainButton
-          backgroundColor="#fff"
-          color="#739c98"
-          border="2px solid #739c98"
-        >
-          إضافة روضة
-        </MainButton>
+        <div>ComponComponentComponentComponentComponentComponent</div>
         <Dropdown overlay={menu} trigger={['click']}>
-          <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-            Click me <DownOutlined />
-          </a>
+          <userImage
+            userName="أﻻء"
+            image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2Fuser&psig=AOvVaw19J8tildYKlGwawGrQb_Hs&ust=1612723904942000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjDxe721e4CFQAAAAAdAAAAABAD"
+          />
+          <DownOutlined />
         </Dropdown>
       </div>
     </div>
   );
 }
-// Navbar.propTypes = {
-//   onClick: PropTypes.func.isRequired,
-// };
+
+Navbar.propTypes = {
+  userName: PropTypes.string.isRequired,
+};
 export default Navbar;
