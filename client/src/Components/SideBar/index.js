b64583ea01d300c0255e3ce6b7f156a45e7c3d10
Osama-you <osama.w.younis@gmail.com>
2021-02-07 13:12:25 +0200

    create sideBar component - import 3 icons (AppstoreAddOutlined ,UnorderedListOutlined ,NotificationOutlined) - I used Menu from antd for side btn - add onClick props - style the compo Relates #46

--- client/src/Components/SideBar/index.js
+++ client/src/Components/SideBar/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Menu, Typography } from 'antd';
+import {
+  AppstoreAddOutlined,
+  UnorderedListOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
 import './style.css';
 
-const SideBar = () => <div />;
+const { Title } = Typography;
+
+const SideBar = ({ onClick }) => (
+  <div className="side-Bar">
+    <Title id="title" level={2}>
+      لوحة التحكم
+    </Title>
+
+    <Menu id="menu" onClick={onClick}>
+      <Menu.Item key="1" icon={<UnorderedListOutlined />}>
+        كل الروضات
+      </Menu.Item>
+      <Menu.Item key="2" icon={<AppstoreAddOutlined />}>
+        إضافة روضة
+      </Menu.Item>
+      <Menu.Item key="3" icon={<NotificationOutlined />}>
+        الإشعارات
+      </Menu.Item>
+    </Menu>
+
+    <Link id="log-out" to="/">
+      سجيل الخروج
+    </Link>
+  </div>
+);
+
+SideBar.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default SideBar;
