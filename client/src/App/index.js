1976357c6b74ec879b5c26b4c3df6f02b55b0fdf
Israa91 <eng.israa.1991@gmail.com>
2021-02-07 13:12:45 +0200

    remove the call of container from app file relates #43

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,100 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CommentContainer from '../Components/Common/CommentContainer';
+
 import './style.css';
 
-const data = [
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 2,
-  },
-  {
-    isAdmin: false,
-    userName: 'هدى',
-    commentText: ' وهذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 5,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 2,
-  },
-  {
-    isAdmin: false,
-    userName: 'هدى',
-    commentText: ' وهذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 5,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-  {
-    isAdmin: true,
-    userName: 'حنين أحمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 2,
-  },
-  {
-    isAdmin: false,
-    userName: 'هدى',
-    commentText: ' وهذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 5,
-  },
-  {
-    isAdmin: true,
-    userName: 'اسراء احمد',
-    commentText: 'هذه روضة رائة اتمنى لها دوام التقدم والنجاح',
-    date: '1 - مايو - 2021',
-    rateValue: 3,
-  },
-];
 const App = () => (
   <Router>
     <div className="App">روضتي هنا</div>
-    <div className="comment-container">
-      <CommentContainer data={data} />
-    </div>
   </Router>
 );
 
