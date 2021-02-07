e5387bf1bbb5d6017f966a8c6017b517dd8e725f
Israa91 <eng.israa.1991@gmail.com>
2021-02-07 09:08:25 +0200

    edit footer styling relates 42

--- client/src/Components/Common/Footer/index.js
+++ client/src/Components/Common/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Layout, Typography, Image } from 'antd';
 
 import facebook from './assets/icons/facebook.svg';
 import instagram from './assets/icons/instagram.svg';
@@ -8,22 +8,29 @@ import twitter from './assets/icons/twitter.svg';
 
 import './style.css';
 
+const { Title } = Typography;
 const { Footer } = Layout;
 const FooterComponent = () => (
-  <Footer className="footer" style={{ padding: '0' }}>
-    <span className="icons">
+  <Footer id="footer">
+    <div className="footer-contents">
       <Link to={{ pathname: 'https://www.instagram.com/' }} target="_blank">
-        <img src={instagram} alt="instagram" className="icon" />
+        <Image
+          preview={false}
+          src={instagram}
+          alt="instagram"
+          className="icon"
+        />
       </Link>
       <Link to={{ pathname: 'https://twitter.com/' }} target="_blank">
-        <img src={twitter} alt="twitter" className="icon" />
+        <Image preview={false} src={twitter} alt="twitter" className="icon" />
       </Link>
       <Link to={{ pathname: 'https://www.facebook.com/' }} target="_blank">
-        <img src={facebook} alt="facebook" className="icon" />
+        <Image preview={false} src={facebook} alt="facebook" className="icon" />
       </Link>
-      <span className="copy-write"> جميع الحقوق محفوظة2021Ⓒ</span>
-    </span>
+      <Title level={5} className="copy-write" style={{ color: '#FFFFFF' }}>
+        جميع الحقوق محفوظة2021Ⓒ
+      </Title>
+    </div>
   </Footer>
 );
-
 export default FooterComponent;
