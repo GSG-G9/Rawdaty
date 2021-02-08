3a280dd67ac459f809d53a5793a0f98d42fca386
Osama-you <osama.w.younis@gmail.com>
2021-02-08 07:49:16 +0200

    Done From Required Changes - useing useHistory to move between routes - add items array that have sideBtns info - in logout from Link to Button Relates #46

--- client/src/Components/Layout/SideBar/index.js
+++ client/src/Components/Layout/SideBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Menu, Typography } from 'antd';
+import { Menu, Typography, Button } from 'antd';
 import {
   AppstoreAddOutlined,
   UnorderedListOutlined,
@@ -11,30 +11,62 @@ import {
 import './style.css';
 
 const { Title } = Typography;
+const { Item } = Menu;
 
-const SideBar = ({ onClick }) => (
-  <div className="side-Bar">
-    <Title id="title" level={2}>
-      لوحة التحكم
-    </Title>
+const SideBar = ({ onClick }) => {
+  const history = useHistory();
 
-    <Menu id="menu" onClick={onClick}>
-      <Menu.Item key="1" icon={<UnorderedListOutlined />}>
-        كل الروضات
-      </Menu.Item>
-      <Menu.Item key="2" icon={<AppstoreAddOutlined />}>
-        إضافة روضة
-      </Menu.Item>
-      <Menu.Item key="3" icon={<NotificationOutlined />}>
-        الإشعارات
-      </Menu.Item>
-    </Menu>
+  const handleItemClick = (to) => {
+    history.push(to);
+  };
 
-    <Link id="log-out" to="/">
-      سجيل الخروج
-    </Link>
-  </div>
-);
+  const handleLogout = () => {
+    // TODO: handle logout logic
+
+    history.push('/');
+  };
+
+  const items = [
+    {
+      name: 'كل الروضات',
+      key: '1',
+      to: '/dashboard/all',
+      icon: <UnorderedListOutlined />,
+    },
+    {
+      name: 'إضافة روضة',
+      key: '2',
+      to: '/dashboard/add',
+      icon: <AppstoreAddOutlined />,
+    },
+    {
+      name: 'الإشعارات',
+      key: '3',
+      to: '/dashboard/notification',
+      icon: <NotificationOutlined />,
+    },
+  ];
+
+  return (
+    <div className="side-Bar">
+      <Title id="title" level={2}>
+        لوحة التحكم
+      </Title>
+
+      <Menu id="menu" onClick={onClick}>
+        {items.map(({ key, icon, to, name }) => (
+          <Item key={key} icon={icon} onClick={() => handleItemClick(to)}>
+            {name}
+          </Item>
+        ))}
+      </Menu>
+
+      <Button id="log-out" type="link" onClick={handleLogout}>
+        تسجيل الخروج
+      </Button>
+    </div>
+  );
+};
 
 SideBar.propTypes = {
   onClick: PropTypes.func.isRequired,
