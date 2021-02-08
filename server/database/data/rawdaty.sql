1c72ecca811abfdd826123280720f69d00f3884e
Israa91 <eng.israa.1991@gmail.com>
2021-02-08 20:35:58 +0200

    create the fake data and make some changes on the scheme relates #74

--- server/database/data/rawdaty.sql
+++ server/database/data/rawdaty.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS kindergartens , users, locations, comments CASCADE;
 
 CREATE TABLE users(
 id SERIAL PRIMARY KEY,
-users_name VARCHAR(50) NOT NULL,
+user_name VARCHAR(50) NOT NULL,
 email VARCHAR(255) NOT NULL,
 password TEXT NOT NULL,
 is_admin VARCHAR(50)  DEFAULT 'false',
@@ -19,17 +19,17 @@ location_name TEXT NOT NULL
 CREATE TABLE kindergartens(
 id SERIAL PRIMARY KEY,
 kindergarten_name VARCHAR(255)	NOT NULL,
-users_id	SERIAL	REFERENCES users(id),
+user_id	INTEGER	REFERENCES users(id),
 cover_image	TEXT NOT NULL,
 description	TEXT NOT NULL,
-location_id	SERIAL	REFERENCES locations(id),
-phone_number INTEGER NOT NULL,
+location_id	INTEGER	REFERENCES locations(id),
+phone_number VARCHAR(10) NOT NULL,
 latitude Decimal(8,6),
 longitude Decimal(9,6),
 min_price FLOAT,	
 max_price FLOAT,	
-periods	TEXT,	
-images_gallery TEXT,	
+periods	TEXT [][],	
+image_gallery TEXT[],	
 request_status VARCHAR(50),	
 is_enable VARCHAR(50)  DEFAULT 'false'
 );
@@ -37,7 +37,7 @@ is_enable VARCHAR(50)  DEFAULT 'false'
 CREATE TABLE comments(
 id SERIAL PRIMARY KEY,
 kindergarten_id	INTEGER REFERENCES kindergartens(id)ON UPDATE CASCADE ON DELETE CASCADE,
-users_name VARCHAR(50) NOT NULL,
+user_name VARCHAR(50) NOT NULL,
 comment	TEXT NOT NULL,
 rating DECIMAL	NOT NULL,
 create_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
