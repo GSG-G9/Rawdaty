2b391c62530d2d533b937d18a27ebb46eaf720ed
Alaa Alser <alaalsser@gmail.com>
2021-02-08 09:48:17 +0200

    using apload component Rleates #44

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Footer from '../Components/Layout/Footer';
+import ImageGallery from '../Components/Common/ImageGallery';
 
 import './style.css';
 
 const App = () => (
   <Router>
-    <Footer />
+    <ImageGallery />
   </Router>
 );
 
--- client/src/Components/Common/ImageGallery/index.js
+++ client/src/Components/Common/ImageGallery/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Image } from 'antd';
+import React, { useState } from 'react';
+import { Upload } from 'antd';
 
-const ImageGallery = ({ images }) => (
-  <>
-    {images.map((img) => (
-      <Image key={img.id} src={img.src} width={100} />
-    ))}
-  </>
-);
+const ImageGallery = () => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url:
+        'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const onPreview = async (file) => {
+    let src = file.url;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow.document.write(image.outerHTML);
+  };
+
+  return (
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture-card"
+      fileList={fileList}
+      onChange={onChange}
+      onPreview={onPreview}
+    >
+      {fileList.length < 5 && '+ Upload'}
+    </Upload>
+  );
+};
 
 export default ImageGallery;
-
-ImageGallery.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
