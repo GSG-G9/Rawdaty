8f8a243c4cb1365e45617a534192f697967ebd83
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-02-09 09:56:44 +0200

    changes in the file adding id for the li tag and some edits in the proptype and defualtprops Relates #36

--- client/src/Components/CardContainer/index.js
+++ client/src/Components/CardContainer/index.js
@@ -1,54 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from 'antd';
+
 import Card from '../Common/Card';
 import './style.css';
 
-const { Title, Text } = Typography;
-const CardContainer = ({ title, data }) => (
+const { Title, Empty } = Typography;
+
+const CardContainer = ({ title, data, searchText }) => (
   <div>
     <Title level={3} id="container-title">
       {title}
+      {searchText && <span className="search-text">{searchText}</span>}
     </Title>
     <div className="container-cards">
       <ul>
         {data.length !== 0 ? (
           data.map((item) => (
-            <li>
+            <li key={item.id}>
               <Card {...item} />
             </li>
           ))
         ) : (
-          <Text>لم يتم إضافة رياض أطفال.</Text>
+          <Empty description="لم يتم إضافة أي رياض أطفال" />
         )}
       </ul>
     </div>
   </div>
 );
+
 CardContainer.defaultProps = {
-  title: ' ',
-  data: [
-    {
-      title: ' ',
-      rating: ' ',
-      reviewersNo: ' ',
-      location: ' ',
-      minPrice: ' ',
-      maxPrice: ' ',
-      cover: ' ',
-    },
-  ],
+  title: '',
+  searchText: '',
+  data: [],
 };
+
 CardContainer.propTypes = {
   title: PropTypes.string,
+  searchText: PropTypes.string,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string,
-      rating: PropTypes.string,
-      reviewersNo: PropTypes.string,
+      rating: PropTypes.number,
+      reviewersNo: PropTypes.number,
       location: PropTypes.string,
-      minPrice: PropTypes.string,
-      maxPrice: PropTypes.string,
+      minPrice: PropTypes.number,
+      maxPrice: PropTypes.number,
       cover: PropTypes.string,
     })
   ),
