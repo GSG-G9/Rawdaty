65aae8f6ea2ac296894b85d187e70d071a12fbcf
Osama-you <osama.w.younis@gmail.com>
2021-02-09 21:08:15 +0200

    crate add kindergartenSearch, valedate the querys and use getKindergartenSearch to get the data Relates #21

--- server/controllers/kindergarten/kindergartenSearch.js
+++ server/controllers/kindergarten/kindergartenSearch.js
@@ -1,3 +1,48 @@
-const kindergartenSearch = () => {};
+const { getKindergartenSearch } = require('../../database/queries');
+const { kindergartenSearchSchema } = require('../../utils/validation');
+const { boomify } = require('../../utils');
+
+const kindergartenSearch = async (req, res, next) => {
+  try {
+    const {
+      q,
+      minPrice,
+      maxPrice,
+      locationId,
+    } = await kindergartenSearchSchema.validate(req.query, {
+      abortEarly: false,
+    });
+
+    if (!q && (!minPrice || !maxPrice) && !locationId) {
+      next(
+        boomify(
+          400,
+          'Validation Error',
+          'At least one of these (q, minPrice & maxPrice,locationId) values is required'
+        )
+      );
+    }
+
+    const { rows: data } = await getKindergartenSearch({
+      q,
+      minPrice,
+      maxPrice,
+      locationId,
+    });
+
+    const StatusCode = 200;
+
+    res.status(StatusCode).json({
+      StatusCode,
+      data,
+    });
+  } catch (error) {
+    next(
+      error.name === 'ValidationError'
+        ? boomify(400, 'Validation Error', error.errors)
+        : error
+    );
+  }
+};
 
 module.exports = kindergartenSearch;
