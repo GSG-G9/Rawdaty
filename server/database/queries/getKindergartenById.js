fc1b3dbe36a03c12071bea091eea8b84b462d9e5
Israa91 <eng.israa.1991@gmail.com>
2021-02-09 14:43:38 +0200

    create the file contains the sql query relates #16

---
+++ server/database/queries/getKindergartenById.js
@@ -0,0 +1,12 @@
+const { connection } = require('../data');
+
+const getKindergartenById = (kindergartenId) => {
+  const sql = {
+    text:
+      'SELECT kindergartens.kindergarten_name,locations.location_sub ,locations.location_main ,comments.comment ,comments.rating FROM  kindergartens ,locations,comments WHERE kindergartens.id=$1 AND kindergartens.location_id =locations.id AND kindergartens.id = comments.kindergarten_id',
+    values: [kindergartenId],
+  };
+  return connection.query(sql);
+};
+
+module.exports = getKindergartenById;
