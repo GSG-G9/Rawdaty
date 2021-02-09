40bf2894b9a57843100e87e7a11352e7d778168b
Israa91 <eng.israa.1991@gmail.com>
2021-02-09 14:58:51 +0200

    change the price values in fake data relates #74

--- server/database/data/rawdatyFake.sql
+++ server/database/data/rawdatyFake.sql
@@ -66,7 +66,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة البسمة الجميلة',2,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/143833048_4097994183544146_3972061880853700220_o.jpg?_nc_cat=111&ccb=2&_nc_sid=e3f864&_nc_ohc=l5Ne-rWVonIAX8y1T2H&_nc_ht=scontent.fgza2-1.fna&oh=8c4be273b40687aecbf709e236db799a&oe=60452CF3',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',2,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',2,'0599123456',500,800,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126071395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127711338_2816934215241119_820060672107449619_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=lj0hKeD_FswAX-IthGI&_nc_ht=scontent.fgza2-1.fna&oh=ff73fc70463202e07ef1ab8945b1aaf6&oe=6047D28E',
@@ -79,7 +79,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة يا هلا النموذجية الخاصة',3,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/145371533_4926517364056683_4419031173382697234_o.jpg?_nc_cat=101&ccb=2&_nc_sid=e3f864&_nc_ohc=p8yB9sYxtWYAX9PP0Ap&_nc_ht=scontent.fgza2-1.fna&oh=ddc08b927f6773710ca251668d628d73&oe=6048B032',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',3,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',3,'0599123456',300,500,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126071395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127711338_2816934215241119_820060672107449619_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=lj0hKeD_FswAX-IthGI&_nc_ht=scontent.fgza2-1.fna&oh=ff73fc70463202e07ef1ab8945b1aaf6&oe=6047D28E',
@@ -92,7 +92,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة الأمل المشرق',4,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/145371533_4926517364056683_4419031173382697234_o.jpg?_nc_cat=101&ccb=2&_nc_sid=e3f864&_nc_ohc=p8yB9sYxtWYAX9PP0Ap&_nc_ht=scontent.fgza2-1.fna&oh=ddc08b927f6773710ca251668d628d73&oe=6048B032',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',4,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',4,'0599123456',1100,1500,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126071395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127711338_2816934215241119_820060672107449619_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=lj0hKeD_FswAX-IthGI&_nc_ht=scontent.fgza2-1.fna&oh=ff73fc70463202e07ef1ab8945b1aaf6&oe=6047D28E',
@@ -118,7 +118,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة الزهراء النموذجية الخاصة',5,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/551059_604585486228232_1415987366_n.jpg?_nc_cat=111&ccb=2&_nc_sid=e3f864&_nc_ohc=kEd6i519NhYAX8iivcY&_nc_ht=scontent.fgza2-1.fna&oh=aa8c6154da2c42b82c50adb9cfa5f5e6&oe=6045F3E5',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',5,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',5,'0599123456',500,1000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126071395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127711338_2816934215241119_820060672107449619_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=lj0hKeD_FswAX-IthGI&_nc_ht=scontent.fgza2-1.fna&oh=ff73fc70463202e07ef1ab8945b1aaf6&oe=6047D28E',
@@ -131,7 +131,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة الحكايات الخاصة',6,'https://scontent.fgza2-1.fna.fbcdn.net/v/t31.0-8/28514422_1621885054527020_2242553309268930940_o.jpg?_nc_cat=100&ccb=2&_nc_sid=e3f864&_nc_ohc=iw4-NXgRvCAAX8x-9zu&_nc_ht=scontent.fgza2-1.fna&oh=3acf28451a6c8f3f1a32d9b8ec609e16&oe=60464007',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',6,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',6,'0599123456',300,700,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126071395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127711338_2816934215241119_820060672107449619_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=lj0hKeD_FswAX-IthGI&_nc_ht=scontent.fgza2-1.fna&oh=ff73fc70463202e07ef1ab8945b1aaf6&oe=6047D28E',
@@ -144,7 +144,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة الأبرار النموذجية',7,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/127563203_2816927261908481_825163598039189311_o.jpg?_nc_cat=100&ccb=2&_nc_sid=e3f864&_nc_ohc=EOam1iTQLIcAX8jrnEj&_nc_ht=scontent.fgza2-1.fna&oh=e1cef81bf1cebbd72a6c1f1af651e01f&oe=604638FB',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',7,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',7,'0599123456',500,800,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126072436_2810042115930329_426976181002437064_o.jpg?_nc_cat=105&ccb=2&_nc_sid=730e14&_nc_ohc=UX2zxNzPBTYAX_XAYLE&_nc_ht=scontent.fgza2-1.fna&oh=72a8ef5d8369f7e183116f4423bad872&oe=604613F1',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p526x395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
@@ -172,7 +172,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة فرح ومرح النموذجية',8,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/20882862_1381420738572793_6405605080998214076_n.jpg?_nc_cat=111&ccb=2&_nc_sid=e3f864&_nc_ohc=xUNX55GZgUQAX9_uQ4i&_nc_ht=scontent.fgza2-1.fna&oh=7653e5f3156f78f449ce341d60ecab0e&oe=6046DE1D',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',8,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',8,'0599123456',800,1200,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126072436_2810042115930329_426976181002437064_o.jpg?_nc_cat=105&ccb=2&_nc_sid=730e14&_nc_ohc=UX2zxNzPBTYAX_XAYLE&_nc_ht=scontent.fgza2-1.fna&oh=72a8ef5d8369f7e183116f4423bad872&oe=604613F1',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p526x395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
@@ -186,7 +186,7 @@ INSERT INTO
 kindergartens (kindergarten_name,user_id,cover_image,description,location_id,phone_number 
 ,min_price,max_price,periods,image_gallery,request_status,is_enable)
 VALUES ('روضة الفجر النموذجية الخاصة',9,'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/603615_564933140213872_176474614_n.jpg?_nc_cat=110&ccb=2&_nc_sid=d2e176&_nc_ohc=FpuEHr77R1MAX-ox9CV&_nc_ht=scontent.fgza2-1.fna&oh=4cc0f90728bdec9ff925b73ad342ac49&oe=6046DED1',
-'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',9,'0599123456',1000,2000,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
+'نعملُ على إنشاء جيل رائع من خلال تطوير كافة مهاراتهم الفكرية والنفسية بأساليب علمية وتربوية',9,'0599123456',400,900,ARRAY[ARRAY['7:00 am','11:00 am'],ARRAY['12:00 pm','3:00 pm']],
 ARRAY['https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-9/126072436_2810042115930329_426976181002437064_o.jpg?_nc_cat=105&ccb=2&_nc_sid=730e14&_nc_ohc=UX2zxNzPBTYAX_XAYLE&_nc_ht=scontent.fgza2-1.fna&oh=72a8ef5d8369f7e183116f4423bad872&oe=604613F1',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p526x395/126362711_2810037339264140_8115186378406081155_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=1pa0nWqLMVQAX8xj-8Z&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=43ba32d04a1478c83ca4cee8950955f9&oe=6045D9DF',
 'https://scontent.fgza2-1.fna.fbcdn.net/v/t1.0-0/p180x540/125246489_2806470342954173_5557632858147020336_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ZGPkuyaJoUEAX9lkFkC&_nc_ht=scontent.fgza2-1.fna&tp=6&oh=0a5795c9d04ed69df0ecf950260a8240&oe=60475C85',
