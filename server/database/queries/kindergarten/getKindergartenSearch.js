6e4e47fc48b2129bb2aa1f06f4d1e4d818ae1aa5
Osama-you <osama.w.younis@gmail.com>
2021-02-09 21:06:10 +0200

    fix code style in getKindergartenSearch quert Relates #21

--- server/database/queries/kindergarten/getKindergartenSearch.js
+++ server/database/queries/kindergarten/getKindergartenSearch.js
@@ -1,17 +1,17 @@
 const connection = require('../../data/connection');
 
 const getKindergartenSearch = ({ q, minPrice, maxPrice, locationId }) => {
-  let sqlText =
+  let text =
     'SELECT kindergartens.id, kindergartens.kindergarten_name, kindergartens.cover_image, kindergartens.phone_number, kindergartens.min_price, kindergartens.max_price, locations.location_sub FROM kindergartens INNER JOIN locations ON locations.id = kindergartens.location_id WHERE ';
   const values = [];
 
   if (q) {
-    sqlText += `kindergarten_name like $${values.length + 1} AND `;
+    text += `kindergarten_name like $${values.length + 1} AND `;
     values.push(q);
   }
 
   if (minPrice && maxPrice) {
-    sqlText += `( min_price <= $${values.length + 1} AND max_price >= $${
+    text += `( min_price <= $${values.length + 1} AND max_price >= $${
       values.length + 2
     }) AND `;
     values.push(minPrice);
@@ -19,18 +19,18 @@ const getKindergartenSearch = ({ q, minPrice, maxPrice, locationId }) => {
   }
 
   if (locationId) {
-    sqlText += `location_id = (SELECT id FROM locations WHERE id = $${
+    text += `location_id = (SELECT id FROM locations WHERE id = $${
       values.length + 1
     }) AND `;
     values.push(locationId);
   }
 
-  const sql = {
-    text: sqlText.slice(0, -4),
-    values,
-  };
+  text = text.slice(0, -4);
 
-  return connection.query(sql);
+  return connection.query({
+    text,
+    values,
+  });
 };
 
 module.exports = getKindergartenSearch;
