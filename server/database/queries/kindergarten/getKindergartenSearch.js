b633c07a96debe4d40ee12c3d863efea8feffa60
Osama-you <osama.w.younis@gmail.com>
2021-02-09 18:12:43 +0200

    add the sql queries Relates #21

--- server/database/queries/kindergarten/getKindergartenSearch.js
+++ server/database/queries/kindergarten/getKindergartenSearch.js
@@ -1,10 +1,35 @@
 const connection = require('../../data/connection');
 
-const getKindergartenSearch = () => {
+const getKindergartenSearch = ({ q, minPrice, maxPrice, locationId }) => {
+  let sqlText =
+    'SELECT kindergartens.id, kindergartens.kindergarten_name, kindergartens.cover_image, kindergartens.phone_number, kindergartens.min_price, kindergartens.max_price, locations.location_sub FROM kindergartens INNER JOIN locations ON locations.id = kindergartens.location_id WHERE ';
+  const values = [];
+
+  if (q) {
+    sqlText += `kindergarten_name like $${values.length + 1} AND `;
+    values.push(q);
+  }
+
+  if (minPrice && maxPrice) {
+    sqlText += `( min_price <= $${values.length + 1} AND max_price >= $${
+      values.length + 2
+    }) AND `;
+    values.push(minPrice);
+    values.push(maxPrice);
+  }
+
+  if (locationId) {
+    sqlText += `location_id = (SELECT id FROM locations WHERE id = $${
+      values.length + 1
+    }) AND `;
+    values.push(locationId);
+  }
+
   const sql = {
-    text: '',
-    values: [],
+    text: sqlText.slice(0, -4),
+    values,
   };
+
   return connection.query(sql);
 };
 
