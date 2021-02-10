8551524bf98928d5457db633b3cb538e753c8841
Osama-you <osama.w.younis@gmail.com>
2021-02-10 15:56:11 +0200

    git anther conf Relates #21

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const kindergarten = require('./kindergarten');
 
-const kindergarten = require('./kindergarten');
-
-router.use(kindergarten);
-
 router.use(kindergarten);
 
 module.exports = router;
