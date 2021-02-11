eedeb48694be1360198adfe03a09d90dfeba0423
Alaa Alser <alaalsser@gmail.com>
2021-02-11 12:28:33 +0200

    edit query Relates #88

--- server/database/queries/locations/getLocations/index.js
+++ server/database/queries/locations/getLocations/index.js
@@ -2,7 +2,7 @@ const connection = require('../../../data/connection');
 
 const getLocatoinsQuery = () => {
   const sql = {
-    text: 'SELECT id,location_sub,location_main FROM locations',
+    text: 'SELECT * FROM locations',
   };
   return connection.query(sql);
 };
