b18081b93fe4b60c03b472faa744950c74471536
Alaa Alser <alaalsser@gmail.com>
2021-02-11 21:59:37 +0200

    Edit controller function Relates #79

--- server/controllers/locations/postLocations/index.js
+++ server/controllers/locations/postLocations/index.js
@@ -1,10 +1,19 @@
 const { postLocationsQuery } = require('../../../database/queries');
+const { locationSchema } = require('../../../utils/validation');
 
 const postLocations = async (req, res, next) => {
   try {
-    const { rows: data } = await postLocationsQuery(req.body);
+    const { subLocation, mainLocation } = req.body;
+    await locationSchema.validate(
+      { subLocation, mainLocation },
+      {
+        abortEarly: false,
+      }
+    );
+    const { rows: data } = await postLocationsQuery(subLocation, mainLocation);
     res.json(201).json({ statusCode: 201, data });
   } catch (err) {
+    console.log(err);
     next(err);
   }
 };
