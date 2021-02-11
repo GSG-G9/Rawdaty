5867fdb37f865dadbd9ee0bb2c7072906ec5c5c5
Alaa Alser <alaalsser@gmail.com>
2021-02-11 19:37:54 +0200

    locatoin quiery Relates #79

--- server/database/queries/locations/postLocations/index.js
+++ server/database/queries/locations/postLocations/index.js
@@ -1,7 +1,11 @@
 const connection = require('../../../data/connection');
 
-const postLocationsQuery = () => {
-  const sql = {};
+const postLocationsQuery = (id, subLocation, mainLocation) => {
+  const sql = {
+    text:
+      'INSERT INTO locations(id,location_sub,location_main) VALUES ($1, $2, $3) RETURNING *',
+    values: [id, subLocation, mainLocation],
+  };
   return connection.query(sql);
 };
 
