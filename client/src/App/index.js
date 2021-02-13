be900509e80daa1541088cb2fc0b8ade652b651b
Osama-you <osama.w.younis@gmail.com>
2021-02-13 10:09:40 +0200

    add /search route Relates #31

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import NavBar from '../Components/Layout/Navbar';
 import Footer from '../Components/Layout/Footer';
+import SearchPage from '../Pages/SearchPage';
 
 import './style.css';
 
 const App = () => (
   <Router>
-    <NavBar userName="Alaa" />
+    <NavBar userName="Alaa" isAdmin={false} />
+    <Switch>
+      <Route exact path="/search">
+        <SearchPage />
+      </Route>
+    </Switch>
     <Footer />
   </Router>
 );
