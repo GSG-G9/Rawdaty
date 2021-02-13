3ff3332037992472e5a91614c869bed29428f9a5
Osama-you <osama.w.younis@gmail.com>
2021-02-13 17:55:55 +0200

    add pagination for CardContainer Relates #31

--- client/src/Components/CardContainer/index.js
+++ client/src/Components/CardContainer/index.js
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Empty } from 'antd';
+import { Typography, Empty, Pagination } from 'antd';
 
 import Card from '../Common/Card';
 import './style.css';
 
 const { Title } = Typography;
 
-const CardContainer = ({ data, searchText }) => (
-  <div>
-    <Title level={3} id="container-title">
-      {searchText ? `نتائج البحث عن: ${searchText}` : `أفضل رياض الأطفال`}
-    </Title>
-    <div className="container-cards">
-      <ul>
-        {data.length !== 0 ? (
-          data.map((item) => (
-            <li key={item.id}>
-              <Card
-                id={item.id}
-                title={item.kindergarten_name}
-                rating={parseFloat(item.rating_average)}
-                reviewersNo={parseFloat(item.rating_count)}
-                location={item.location_sub}
-                minPrice={item.min_price}
-                maxPrice={item.max_price}
-                cover={item.cover_image}
-              />
-            </li>
-          ))
-        ) : (
-          <Empty description="لا يوجد رياض أطفال" />
-        )}
-      </ul>
+const CardContainer = ({ data, searchText }) => {
+  const numEachPage = 9;
+  const [limit, setLimit] = useState([0, numEachPage]);
+  return (
+    <div>
+      <Title level={3} id="container-title">
+        {searchText ? `نتائج البحث عن: ${searchText}` : `أفضل رياض الأطفال`}
+      </Title>
+      <div className="container-cards">
+        <ul>
+          {data.length !== 0 ? (
+            data.slice(limit[0], limit[1]).map((item) => (
+              <li key={item.id}>
+                <Card
+                  id={item.id}
+                  title={item.kindergarten_name}
+                  rating={parseFloat(item.rating_average)}
+                  reviewersNo={parseFloat(item.rating_count)}
+                  location={item.location_sub}
+                  minPrice={item.min_price}
+                  maxPrice={item.max_price}
+                  cover={item.cover_image}
+                />
+              </li>
+            ))
+          ) : (
+            <Empty description="لا يوجد رياض أطفال" />
+          )}
+        </ul>
+        <Pagination
+          total={data.length}
+          onChange={(value) => {
+            setLimit([(value - 1) * numEachPage, value * numEachPage]);
+          }}
+          showSizeChanger={false}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 CardContainer.defaultProps = {
   searchText: '',
