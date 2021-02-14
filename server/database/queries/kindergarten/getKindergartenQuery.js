6a20c387b13aac42d22c23e3b59f41f7bfecfcce
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2021-02-14 15:35:07 +0200

    fix getKindergartensQuery

--- server/database/queries/kindergarten/getKindergartenQuery.js
+++ server/database/queries/kindergarten/getKindergartenQuery.js
@@ -3,7 +3,7 @@ const connection = require('../../data/connection');
 const getKindergartenQuery = () => {
   const sql = {
     text:
-      `SELECT *, locations.location_sub, locations.location_main, ` +
+      `SELECT kindergartens.*, locations.location_sub, locations.location_main, ` +
       `(SELECT AVG(comments.rating) AS rating_average FROM comments WHERE ` +
       `comments.kindergarten_id = kindergartens.id), (SELECT COUNT(comments) AS rating_count FROM ` +
       `comments WHERE comments.kindergarten_id = kindergartens.id) FROM kindergartens INNER JOIN ` +
