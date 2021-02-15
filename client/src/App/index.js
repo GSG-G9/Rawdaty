7daf0a3de67c4c9f301d86a0f31f90728ac63136
Osama-you <osama.w.younis@gmail.com>
2021-02-15 11:09:42 +0200

    add the left image Relates #30

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../Components/Layout/Navbar';
 import Footer from '../Components/Layout/Footer';
 import Home from '../Pages/HomePage';
+import LoginPage from '../Pages/LoginPage';
+
 import './style.css';
 
 const App = () => (
   <Router>
-    <NavBar userName="Alaa" isAdmin={false} />
     <Switch>
+      <Route exact to="/login">
+        <LoginPage />
+      </Route>
       <Route exact to="/">
+        <NavBar userName="Alaa" isAdmin={false} />
         <Home />
+        <Footer />
       </Route>
     </Switch>
-    <Footer />
   </Router>
 );
 
---
+++ client/src/Pages/LoginPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+import './style.css';
+
+const LoginPage = () => (
+  <Row>
+    <Col id="form-container" span={12}>
+      col-12
+    </Col>
+    <Col id="left-image" span={12} />
+  </Row>
+);
+
+export default LoginPage;
