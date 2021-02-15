8433f472656ab3161d8e1ddc975c3d078f763a27
Alaa Alser <alaalsser@gmail.com>
2021-02-15 12:52:16 +0200

    add delete route Relates #5

--- server/router/kindergarten.js
+++ server/router/kindergarten.js
@@ -4,10 +4,12 @@ const {
   getKindergarten,
   getSpecificKindergarten,
   kindergartenSearch,
+  deleteKindergarten,
 } = require('../controllers');
 
 router.get('/kindergarten', getKindergarten);
 router.get('/kindergarten/:kindergartenId', getSpecificKindergarten);
 router.get('/search', kindergartenSearch);
+router.delete('/kindergarten/:kindergartenId', deleteKindergarten);
 
 module.exports = router;
