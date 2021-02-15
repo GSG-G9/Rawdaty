ad4bdb785478dbfb997f5d60f9837c59b5aa5f3a
Osama-you <osama.w.younis@gmail.com>
2021-02-15 09:48:15 +0200

    add the error into login Relates #17

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,20 +1,22 @@
 const { compare } = require('bcrypt');
 const { sign } = require('../../utils/jwt');
+const boomify = require('../../utils/boomify');
 const { checkEmail } = require('../../database/queries');
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const { rows: user } = await checkEmail({ email });
 
+    const { rows: user } = await checkEmail({ email });
     if (!user[0]) {
-      return next();
+      return next(boomify(400, 'Login Error', 'Bad Request'));
     }
+
     const { id: userId, password: userPassword } = user[0];
 
     const isPassword = await compare(password, userPassword);
     if (!isPassword) {
-      return next();
+      return next(boomify(400, 'Login Error', 'Bad Request'));
     }
 
     const token = await sign({ userId });
