eba82c0355a268f9981791875bdc0a4df88474e0
Osama-you <osama.w.younis@gmail.com>
2021-02-15 09:46:05 +0200

    add antd & boomify into cSpell.words Relates #17

--- server/router/user.js
+++ server/router/user.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const { getUsers, login } = require('../controllers');
 
 router.get('/users', getUsers);
-router.get('/login', login);
+router.post('/login', login);
 
 module.exports = router;
