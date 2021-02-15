91e610d68419325521727636ed1ca9dec7f19aaa
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2021-02-15 17:55:31 +0200

    Remove console.log from server error

--- server/controllers/errorHandle/serverError.js
+++ server/controllers/errorHandle/serverError.js
@@ -2,8 +2,6 @@ const { boomify } = require('../../utils');
 
 // eslint-disable-next-line no-unused-vars
 const serverError = (err, req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log(err);
   const errorMessage = err.statusCode
     ? err
     : boomify(500, 'Internal Server Error', 'Something went wrong');
