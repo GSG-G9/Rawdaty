91546c17922baba1a65a40d3cc0b56a5e9679c10
Osama-you <osama.w.younis@gmail.com>
2021-02-16 11:49:42 +0200

    made the style for the login page Relates #30

--- client/src/Pages/LoginPage/index.js
+++ client/src/Pages/LoginPage/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { NavLink } from 'react-router-dom';
+import { Row, Col, Typography } from 'antd';
+import MainInput from '../../Components/Common/MainInput';
+import MainButton from '../../Components/Common/MainButton';
 
 import './style.css';
 
+const { Title } = Typography;
+
 const LoginPage = () => (
-  <Row>
+  <Row id="Login-container">
     <Col id="form-container" span={12}>
-      col-12
+      <Title id="title" level={1}>
+        روضتـــي
+      </Title>
+      <Title level={1} style={{ marginTop: 0, color: '#5A5A5A' }}>
+        مرحبا بعودتك
+      </Title>
+      <MainInput
+        type="text"
+        textLabel="البريد الإلكتروني"
+        placeholder="example@example.com"
+        width="470px"
+        height="60px"
+        id="input"
+      />
+      <MainInput
+        type="password"
+        textLabel="كلمة المرور"
+        placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+        width="470px"
+        height="60px"
+        id="input"
+      />
+      <MainButton id="login-btn" width="470px" height="55px">
+        تسجيل الدخول
+      </MainButton>
+      <div className="sign-up">
+        <NavLink className="sign-up-link" to="/sign-up">
+          إنشاء حساب جديد
+        </NavLink>
+      </div>
     </Col>
     <Col id="left-image" span={12} />
   </Row>
